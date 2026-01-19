d49e578034ee088131a5c5c7f1188225c2bc2bf6
Hagen Schlotzhauer <jan.schlotzhauer@fiscalnote.com>
2026-01-20 00:38:48 +0100

    working stock picker

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,8 +2,8 @@ from flask import jsonify, render_template, request, redirect, url_for, current_
 from flask_login import login_required, current_user
 from app import db
 from app.main import bp
-from app.models import Prediction, Category, User
-from app.utils.stocks import search_stock_ticker
+from app.models import Prediction, Category, User, StockPick, StockUpdate
+from app.utils.stocks import search_stock_ticker, get_stock_info
 
 from sqlalchemy import func
 from collections import defaultdict
@@ -29,6 +29,7 @@ def live_game():
 @login_required
 def prediction_overview():
     predictions = Prediction.query.filter_by(user_id=current_user.id).all()
+    stockpicks = StockPick.query.filter_by(user_id=current_user.id).all()
     user_ids = [2, 3, 4, 5, 6, 7]
 
     results = db.session.query(
@@ -46,7 +47,7 @@ def prediction_overview():
         sorted_predictions[p.category.name].append(p)
     current_app.logger.info(sorted_predictions)
 
-    return render_template('make_predictions.html', title='2026 Predictions', predictions=sorted_predictions, progress=counts_dict, name=current_user.user_name)
+    return render_template('make_predictions.html', title='2026 Predictions', predictions=sorted_predictions, stockpicks=stockpicks, progress=counts_dict, name=current_user.user_name)
 
 
 @bp.route('/api/stocks/search', methods=['POST'])
@@ -78,7 +79,7 @@ def create_prediction():
         if field not in data:
             return jsonify({'error': f'Missing required field: {field}'}), 400
 
-    prediction = Prediction(user_id=current_user.id, title=data.get("title"), description=data.get("description"), category=Category[data.get("category")])
+    prediction = Prediction(user_id=data.get("user_id", current_user.id), title=data.get("title"), description=data.get("description"), category=Category[data.get("category")])
     db.session.add(prediction)
     db.session.commit()
 
@@ -141,4 +142,113 @@ def delete_project(prediction_id):
         'message': 'Prediction successfully deleted',
         'id': prediction.id,
         'data': prediction.to_dict()
+    }), 200
+
+@bp.route('/api/stockpicks', methods=['POST'])
+def create_stockpick():
+    """Create a new stockpick"""
+    data = request.get_json()
+
+    # Validate required fields
+    required_fields = ['symbol']
+    for field in required_fields:
+        if field not in data:
+            return jsonify({'error': f'Missing required field: {field}'}), 400
+    
+    stock_info = get_stock_info(data["symbol"])
+    
+    if not stock_info:
+        return jsonify({'error': f'Error fetching info for: {data["symbol"]}'}), 400
+    pick = StockPick(
+        user_id=data.get("user_id", current_user.id),
+        symbol=data["symbol"],
+        name=stock_info[0]["companyName"],
+        currency=stock_info[0]["currency"],
+        country=stock_info[0]["country"],
+        ceo=stock_info[0]["ceo"],
+        exchange_full_name=stock_info[0]["exchangeFullName"],
+        exchange=stock_info[0]["exchange"],
+        sector=stock_info[0]["sector"],
+        industry=stock_info[0]["industry"],
+        employees=stock_info[0]["fullTimeEmployees"],
+        description=stock_info[0]["description"],
+        website=stock_info[0]["website"],
+        image=stock_info[0]["image"],
+        initial_price=stock_info[0]["price"],
+        current_price=stock_info[0]["price"]
+    )
+    db.session.add(pick)
+    db.session.flush()
+    update = StockUpdate(
+        stock_id=pick.id,
+        price=stock_info[0]["price"],
+        change=stock_info[0]["change"],
+        change_percentage=stock_info[0]["changePercentage"],
+        beta=stock_info[0]["beta"],
+        market_cap=stock_info[0]["marketCap"],
+        volume=stock_info[0]["volume"],
+        average_volume=stock_info[0]["averageVolume"]
+    )
+    db.session.add(update)
+    db.session.commit()
+
+    return jsonify({
+        'message': 'Stock successfully added',
+        'id': pick.id,
+        'data': pick.to_dict()
+    }), 200
+
+@bp.route('/api/stockpicks', methods=['GET'])
+@login_required
+def fetch_stockpicks():
+    """Fetch predictions"""
+
+    #Pulling the data from the URL for further use
+    user = current_user
+    category = request.args.get('category', None)
+
+    predictions = Prediction.query.filter_by(user_id=user.id).all()
+    data = []
+    for x in predictions:
+        data.append(x.to_dict())
+
+    return jsonify({
+        'message': 'Search successfull',
+        'id': None,
+        'data': data
+    }), 200
+
+@bp.route('/api/stockpicks/<int:stock_id>', methods=['PUT', 'PATCH'])
+def update_stockpicks(stock_id):
+    """Update an existing prediction"""
+    prediction = Prediction.query.get_or_404(prediction_id)
+    data = request.get_json()
+    
+    # Update fields if provided
+    if 'title' in data:
+        prediction.title = data['title']
+    if 'subtitle' in data:
+        prediction.description = data['description']
+    if 'category' in data:
+        prediction.category = Category[data['category']]
+    
+    db.session.commit()
+    
+    return jsonify({
+        'message': 'Prediction successfully updated',
+        'id': prediction.id,
+        'data': prediction.to_dict()
+    }), 200
+
+@bp.route('/api/stockpicks/<int:stock_id>', methods=['DELETE'])
+def delete_stockpick(stock_id):
+    """Delete a prediction"""
+    pick = StockPick.query.get_or_404(stock_id)
+    db.session.delete(pick)
+    db.session.commit()
+    
+    return jsonify({
+        'message': 'Stock successfully deleted',
+        'id': pick.id,
+        'data': pick.to_dict()
     }), 200
--- app/models.py
+++ app/models.py
@@ -43,29 +43,57 @@ class Prediction(UserMixin, db.Model):
          "created_at": self.created_at}
         return content
 
-class StockPick(UserMixin, db.Model):
+class StockPick(db.Model):
     __tablename__ = "stockpick"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    symbol = db.Column(db.String(12), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), index=True, nullable=False)
+    symbol = db.Column(db.String(12), index=True, nullable=False)
     name = db.Column(db.String(128), nullable=False)
     currency = db.Column(db.String(12), nullable=False)
+    country = db.Column(db.String(12), nullable=False)
+    ceo = db.Column(db.String(64), nullable=False)
     exchange_full_name = db.Column(db.String(128), nullable=False)
     exchange = db.Column(db.String(12), nullable=False)
+    sector = db.Column(db.String(64), nullable=False)
+    industry = db.Column(db.String(128), nullable=False)
+    employees = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text, nullable=True)
-    category = db.Column(db.Enum(Category), nullable=False)
-    created_at = db.Column(db.Date, default= lambda: datetime.now(timezone.utc))
+    website = db.Column(db.String(128), nullable=False)
+    image = db.Column(db.String(128), nullable=False)
+    last_checked = db.Column(db.Date, default= lambda: datetime.now(timezone.utc))
+    initial_price = db.Column(db.Float, nullable=False)
+    current_price = db.Column(db.Float, nullable=False)
 
     __table_args__ = (
             db.UniqueConstraint('user_id', 'symbol', name='_user_symbol_uc'),
         )
 
     def to_dict(self):
-        content = {"title": self.title,
+        content = {"name": self.name,
+         "symbol": self.symbol,
          "description": self.description,
-         "category": self.category.value,
-         "created_at": self.created_at}
+         "sector": self.sector,
+         "industry": self.industry,
+         "price": self.current_price,
+         "last_checked": self.last_checked}
         return content
+
+    @property
+    def total_return(self):
+        return ((self.current_price - self.initial_price) / self.initial_price) * 100
+
+class StockUpdate(db.Model):
+    __tablename__ = "stockupdate"
+    id = db.Column(db.Integer, primary_key=True)
+    stock_id = db.Column(db.Integer, db.ForeignKey("stockpick.id"), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    change = db.Column(db.Float, nullable=False)
+    change_percentage = db.Column(db.Float, nullable=False)
+    beta = db.Column(db.Float, nullable=False)
+    market_cap = db.Column(db.BigInteger, nullable=False)
+    volume = db.Column(db.Integer, nullable=False)
+    average_volume = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default= lambda: datetime.now(timezone.utc))
 
 
 @login.user_loader
